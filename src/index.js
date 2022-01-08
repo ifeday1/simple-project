fb92cf068590c29fbd054ece91f5009af7656732
ifeday1 <oladeniifeoluwa123@gmail.com>
2022-01-08 14:39:56 +0100

    tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-function Greeting(){
-  return <h4>This is if
-    
-    
-    e and this is my first component</h4>;
+// function Greeting(){
+//   return (
+//   <div>
+//   <h4>This is ife and this is my first component</h4>
+//   </div>
+//   );
+// };
+
+const Greeting = () => {
+return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'))
 }
 ReactDom.render(<Greeting/>, document.getElementById("root"));
