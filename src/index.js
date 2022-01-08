3ebcaf8f110b7ef1835c6c6f48e0a550700ec23e
ifeday1 <oladeniifeoluwa123@gmail.com>
2022-01-08 14:00:49 +0100

    tutorail

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom'
 
 function Greeting(){
-  return <h4>This is ife and this is my first component</h4>;
+  return <h4>This is if
+    
+    
+    e and this is my first component</h4>;
 }
 ReactDom.render(<Greeting/>, document.getElementById("root"));
