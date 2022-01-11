86fceba4e1631dfab8a137c84e74c56493e210c6
ifeday1 <oladeniifeoluwa123@gmail.com>
2022-01-11 15:14:34 +0100

    props destructuring

--- src/index.js
+++ src/index.js
@@ -25,15 +25,13 @@ function BookList(){
   );
 
 };
-
-
-const Book = (props) => {
- console.log(props)
+const Book = ({img,title,author}) => {
+//  const {img, title, author}= props
 return(
   <article className="books">
-    <img src= {props.img} alt=""/>
-    <h1>{props.title}</h1>
-    <h4> {props.author} </h4>
+    <img src= {img} alt=""/>
+    <h1>{title}</h1>
+    <h4> {author} </h4>
     
 
     </article>
